bf7958ef3e4a3e5e041d4b685f41b9e4268b838d
SongHyunmsoo <y85858@naver.com>
2023-12-01 10:05:40 +0900

    12/1일 수업1 빌드 + 사용법 레코드 클래스

--- 2. 서블릿& JSP/exam02/src/main/java/exam03/A.java
+++ 2. 서블릿& JSP/exam02/src/main/java/exam03/A.java
@@ -7,5 +7,6 @@ public class A {
     public A() {
         super();
         System.out.println("A 생성자!");
+
     }
 }
---
+++ 2. 서블릿& JSP/exam02/src/main/java/exam04/Ex01.java
@@ -0,0 +1,13 @@
+package exam04;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        Book b1 = new Book();
+        Book b1 = Book.builder()
+                .title("책2")
+                .author("저자2")
+                .publisher("출판사2")
+                .build();
+        System.out.println(b1);
+    }
+}
