9c2dcaaa6d5e79cb57a12b6a13110ce47b93a803
SongHyunmsoo <y85858@naver.com>
2023-12-04 12:12:29 +0900

    2023/12/04 서블릿 3

--- 2. 서블릿& JSP/exam04/src/main/java/servlets/LoginServlet.java
+++ 2. 서블릿& JSP/exam04/src/main/java/servlets/LoginServlet.java
@@ -1,5 +1,6 @@
 package servlets;
 
+import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -8,11 +9,24 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet("/member/login")
+//@WebServlet("/member/login")
 public class LoginServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("로그인 페이지!!!");
+        System.out.println("doGet()!!!!");
 
     }
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        System.out.println("init()!!!!!");
+        String key1 = config.getInitParameter("key1");
+        String key2 = config.getInitParameter("key2");
+        System.out.printf("key1=%s,key2=%s%n",key1,key2);
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy()!!!");
+    }
 }
+
--- 2. 서블릿& JSP/exam04/src/main/webapp/jspex/ex02.jsp
+++ 2. 서블릿& JSP/exam04/src/main/webapp/jspex/ex02.jsp
@@ -1,5 +1,13 @@
 <%@ page contentType="text/html; charset=UTF-8"%>
 
 <%
-    response.sendRedirect("ex03.jsp");
+    // _jspService 지역 내부
+
+    //response.sendRedirect("ex03.jsp");
+    out.write("<h1>ex02.jsp");
+%>
+
+<%!
+    // 선언문에서는 내장 객체 접근 불가..
+
 %>
