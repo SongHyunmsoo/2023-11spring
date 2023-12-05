2fb6e7485d58a70e08fbda40a80c8f842b8562f3
SongHyunmsoo <y85858@naver.com>
2023-12-05 11:01:47 +0900

    2023/12/05 2 예외처리

--- 3.스프링/spring_study/exam01/src/main/java/models/member/JoinValidator.java
+++ 3.스프링/spring_study/exam01/src/main/java/models/member/JoinValidator.java
@@ -1,4 +1,20 @@
-package models.member;
+        package models.member;
 
-public class JoinValidator {
+import commons.validators.Validator;
+
+public class JoinValidator implements Validator<Member> {
+
+    public void validate(Member member) {
+        // 전달받은 객체인 Member member를 검증
+        /* 필수 항목 검증(아이디, 비밀번호, 비밀번호 확인, 회원명 */
+        String userId = member.getUserId();
+        String userPw = member.getUserPw();
+        String confirmPw = member.getConfirmPw();
+        String userNm = member.getUserNm();
+
+        if(userId == null || userId.isBlank()) {
+
+        }
+
+    }
 }
