4dabf35c004cf667bea8151e299437309363cdda
SongHyunmsoo <y85858@naver.com>
2023-12-11 11:19:39 +0900

    2023/12/11 JdbcTemplate2

--- 3.스프링/spring_study/exam06/src/test/java/jdbctest/JdbcTemplateTest.java
+++ 3.스프링/spring_study/exam06/src/test/java/jdbctest/JdbcTemplateTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -13,22 +14,36 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = AppCtx.class)
+@ContextConfiguration(classes= AppCtx.class)
 public class JdbcTemplateTest {
 
     @Autowired
     private DataSource dataSource;
 
-    @Test
-    @DisplayName("DataSource를 통한 db 연결 테스트")
-    void connectionTest() {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
+    @Test
+    @DisplayName("DataSource를 통한 DB 연결 테스트")
+    void connectionTest() {
         try (Connection conn = dataSource.getConnection()) {
             System.out.println(conn);
-        }catch (SQLException e) {
+
+        } catch (SQLException e) {
             e.printStackTrace();
         }
-
     }
 
+    @Test
+    @DisplayName("INSERT 테스트")
+    void insertTest() {
+        // DataAccessException - RuntimeException - 예외처리 X -> 실행
+
+        String sql = "INSERT INTO MEMBER (USER_NO, USER_ID, USER_PW, USER_NM, EMAIL) " +
+                " VALUES (SEQ_MEMBER.nextval, ?, ?, ?, ?)";
+        int affectedRows = jdbcTemplate.update(sql,
+                "USER101", "123456", "사용자101", "user101@test.org");
+
+        System.out.println(affectedRows);
+    }
 }
