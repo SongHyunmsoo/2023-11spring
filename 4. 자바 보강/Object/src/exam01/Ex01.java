91805e1b47c9af1b96857f5d1172dbac73a87e0f
SongHyunmsoo <y85858@naver.com>
2023-12-19 19:46:56 +0900

    2023 /12/19 객체란

--- 4. 자바 보강/Object/src/exam01/Ex01.java
+++ 4. 자바 보강/Object/src/exam01/Ex01.java
@@ -2,9 +2,11 @@ package exam01;
 
 public class Ex01 {
     public static void main(String[] args) {
+        /*
         Student s1 = new Student(); // 생성자 메서드: 객체를 생성하는 역활
         s1.id = 1000;
         s1.name = "학생1";
         s1.subject = "과목1";
+*/
     }
 }
--- 4. 자바 보강/Object/src/exam01/Student.java
+++ 4. 자바 보강/Object/src/exam01/Student.java
@@ -1,15 +1,24 @@
 package exam01;
 
 public class Student {
-    int id ;
+    static int id ;
     String name;
     String subject;
-
+/**
     public Student(){
         // 멤버변수의 초기화 작업을 주로 진행 
-    }                   // 컴파일러가 목적을 수행하기 위해 만들어놓은 기본 생성자
+        id = 1000;
+        name = "이름1";
+        subject = "과목1";
+
+    } */                  // 컴파일러가 목적을 수행하기 위해 만들어놓은 기본 생성자
                         // 반환값은 주소값
 
+    public Student(int _id,String _name,String _subject){
+        id= _id;
+        name =_name;
+        subject =_subject;
+    }
     void showInfo(){
         System.out.printf("id=%d,name%s,subject=%s%n"
         ,id,name,subject);
