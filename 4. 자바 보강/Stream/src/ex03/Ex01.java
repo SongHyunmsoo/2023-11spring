83758309a3ceac1d03de719362e9d406ad44f871
SongHyunmsoo <y85858@naver.com>
2023-12-19 17:08:48 +0900

    2023/12/19 자바 스트림

---
+++ 4. 자바 보강/Stream/src/ex03/Ex01.java
@@ -0,0 +1,9 @@
+package ex03;
+
+import java.util.stream.IntStream;
+
+public class Ex01 {
+    public static void main(String[] args) {
+      //  IntStream.range
+}
+}
---
+++ 4. 자바 보강/Stream/src/ex03/Ex03.java
@@ -0,0 +1,24 @@
+package ex03;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Ex03 {
+    public static void main(String[] args) {
+        List<Book> books = Arrays.asList(
+                new Book("책1","저자1","출판사1"),
+                new Book("책2","저자2","출판사2"),
+                new Book("책2","저자2","출판사2"),
+                new Book("책3","저자3","출판사3"),
+                new Book("책4","저자4","출판사4")
+
+        );
+
+        books.stream().
+                distinct()
+                .sorted(Comparator.reverseOrder())
+                .forEach((System.out::println));
+
+    }
+}
