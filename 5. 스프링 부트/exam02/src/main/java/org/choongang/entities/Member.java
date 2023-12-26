cdd696a6db5345daed13694b1695d8d81e1132b9
SongHyunmsoo <y85858@naver.com>
2023-12-26 10:15:40 +0900

    2023-12/26 JPA

--- 5. 스프링 부트/exam02/src/main/java/org/choongang/entities/Member.java
+++ 5. 스프링 부트/exam02/src/main/java/org/choongang/entities/Member.java
@@ -1,21 +1,20 @@
 package org.choongang.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
 @Entity
+@Table(name="USERS",
+        indexes = @Index(name="idx_member_createdAt", columnList = "createdAt DESC"))
 public class Member {
-    @Id
+    @Id @GeneratedValue
     private Long seq;
     private String email;
     private String name;
     private String password;
     private LocalDateTime createdAt;
     private LocalDateTime modifiedAt;
-
 }
